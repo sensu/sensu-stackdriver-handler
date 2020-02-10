88f134b5b735a94c0dfa464f4c9eeb7d9dddfb63
Sean Porter <portertech@gmail.com>
2020-02-10 13:22:31 -0800

    handler structure, gcp stackdriver write time series value
    Signed-off-by: Sean Porter <portertech@gmail.com>

--- go.mod
+++ go.mod
@@ -1,8 +1,13 @@
-module github.com/{{ .GithubUser }}/{{ .GithubProject }}
+module github.com/portertech/sensu-stackdriver-handler
 
 go 1.13
 
 require (
+	cloud.google.com/go v0.38.0
+	github.com/golang/protobuf v1.3.2
+	github.com/googleapis/gax-go v2.0.2+incompatible // indirect
 	github.com/sensu-community/sensu-plugin-sdk v0.5.0-alpha3
 	github.com/sensu/sensu-go v0.0.0-20190508172758-32aea478ae74
+	google.golang.org/api v0.17.0 // indirect
+	google.golang.org/genproto v0.0.0-20190819201941-24fa4b261c55
 )
--- main.go
+++ main.go
@@ -1,46 +1,53 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"time"
 
+	monitoring "cloud.google.com/go/monitoring/apiv3"
+	timestamp "github.com/golang/protobuf/ptypes/timestamp"
 	"github.com/sensu-community/sensu-plugin-sdk/sensu"
 	"github.com/sensu/sensu-go/types"
+	metricpb "google.golang.org/genproto/googleapis/api/metric"
+	monitoredres "google.golang.org/genproto/googleapis/api/monitoredres"
+	monitoringpb "google.golang.org/genproto/googleapis/monitoring/v3"
 )
 
 type HandlerConfig struct {
 	sensu.PluginConfig
-	Example string
+	ProjectID string
 }
 
 type ConfigOptions struct {
-	Example sensu.PluginConfigOption
+	ProjectID sensu.PluginConfigOption
 }
 
 var (
 	handlerConfig = HandlerConfig{
 		PluginConfig: sensu.PluginConfig{
-			Name:     "{{ .Name }}",
-			Short:    "{{ .Description }}",
+			Name:     "sensu-stackdriver-handler",
+			Short:    "Send Sensu Go collected metrics to Google Stackdriver",
 			Timeout:  10,
-			Keyspace: "sensu.io/plugins/{{ .Name }}/config",
+			Keyspace: "sensu.io/plugins/sensu-stackdriver-handler/config",
 		},
 	}
 
 	handlerConfigOptions = ConfigOptions{
-		Example: sensu.PluginConfigOption{
-			Path:      "example",
-			Env:       "HANDLER_EXAMPLE",
-			Argument:  "example",
-			Shorthand: "e",
+		ProjectID: sensu.PluginConfigOption{
+			Path:      "project-id",
+			Env:       "STACKDRIVER_PROJECTID",
+			Argument:  "project-id",
+			Shorthand: "p",
 			Default:   "",
-			Usage:     "An example configuration option",
-			Value:     &handlerConfig.Example,
+			Usage:     "The Google Cloud Project ID",
+			Value:     &handlerConfig.ProjectID,
 		},
 	}
 
 	options = []*sensu.PluginConfigOption{
-		&handlerConfigOptions.Example,
+		&handlerConfigOptions.ProjectID,
 	}
 )
 
@@ -50,13 +57,56 @@ func main() {
 }
 
 func checkArgs(_ *types.Event) error {
-	if len(handlerConfig.Example) == 0 {
-		return fmt.Errorf("--example or HANDLER_EXAMPLE environment variable is required")
+	if len(handlerConfig.ProjectID) == 0 {
+		return fmt.Errorf("--project-id or STACKDRIVER_PROJECTID environment variable is required")
+	}
+	return nil
+}
+
+func writeTimeSeriesValue(projectID, metricType string, value int) error {
+	ctx := context.Background()
+	c, err := monitoring.NewMetricClient(ctx)
+	if err != nil {
+		return err
+	}
+	now := &timestamp.Timestamp{
+		Seconds: time.Now().Unix(),
+	}
+	req := &monitoringpb.CreateTimeSeriesRequest{
+		Name: "projects/" + handlerConfig.ProjectID,
+		TimeSeries: []*monitoringpb.TimeSeries{{
+			Metric: &metricpb.Metric{
+				Type: metricType,
+			},
+			Resource: &monitoredres.MonitoredResource{
+				Type: "gce_instance",
+				Labels: map[string]string{
+					"project_id": handlerConfig.ProjectID,
+				},
+			},
+			Points: []*monitoringpb.Point{{
+				Interval: &monitoringpb.TimeInterval{
+					StartTime: now,
+					EndTime:   now,
+				},
+				Value: &monitoringpb.TypedValue{
+					Value: &monitoringpb.TypedValue_Int64Value{
+						Int64Value: int64(value),
+					},
+				},
+			}},
+		}},
+	}
+	log.Printf("writeTimeseriesRequest: %+v\n", req)
+
+	err = c.CreateTimeSeries(ctx, req)
+	if err != nil {
+		return fmt.Errorf("could not write time series value, %v ", err)
 	}
 	return nil
 }
 
 func executeHandler(event *types.Event) error {
-	log.Println("executing handler with --example", handlerConfig.Example)
+	log.Println("executing handler with --project-id", handlerConfig.ProjectID)
 	return nil
 }
